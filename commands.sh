a55d7047aa2f05ff21fb4f9d9e45387b76c1c2ec
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-07-03 22:35:56 +0000

    update

--- commands.sh
+++ commands.sh
@@ -1,2 +1,12 @@
-#2 +a -f-/opt/dlami/nvme/smoke_test_outputs_v3/V6f_128.log,/opt/dlami/nvme/smoke_test_outputs_v3/V5_mid10.log,/opt/dlami/nvme/smoke_test_outputs_v3/V3_emb.log,/opt/dlami/nvme/smoke_test_outputs_v3/V2_emb.log,/opt/dlami/nvme/smoke_test_outputs_v3/V6f_128/smoke_metrics.csv,/opt/dlami/nvme/smoke_test_outputs_v3/V5_mid10/smoke_metrics.csv,/opt/dlami/nvme/smoke_test_outputs_v3/V3_emb/smoke_metrics.csv,/opt/dlami/nvme/smoke_test_outputs_v3/V2_emb/smoke_metrics.csv
-#train-v3-logs
+#1
+#train-v2c-tail
+eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
+sleep 3
+conda activate embeddings_hub
+sleep 3
+export WANDB_MODE=offline
+
+nvidia-smi
+sleep 3
+
+python run_smoke_tests_v3.py --arms V2c_tail_emb --save-token-ids --stop-at-step 6500
