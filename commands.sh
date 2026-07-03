5a6f39d305f0dc9cd3fe373375b248c12bd92f52
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-07-03 06:14:00 +0000

    update

--- commands.sh
+++ commands.sh
@@ -1,2 +1,2 @@
-#2 +a
-#train-v3-variants
+#2 +a -f-/opt/dlami/nvme/smoke_test_outputs_v3/V6f_128.log,/opt/dlami/nvme/smoke_test_outputs_v3/V5_mid10.log,/opt/dlami/nvme/smoke_test_outputs_v3/V6f_128/smoke_metrics.csv,/opt/dlami/nvme/smoke_test_outputs_v3/V5_mid10/smoke_metrics.csv
+#train-v3-logs
