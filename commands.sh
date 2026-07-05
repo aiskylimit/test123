6e0bc64d503fec928f5bb74b36a3ad3e32a7da57
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-07-05 14:45:10 +0000

    update

--- commands.sh
+++ commands.sh
@@ -1,13 +1,2 @@
-#1 +60+a
-#train-linear-ablation
-eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
-sleep 3
-conda activate embeddings_hub
-sleep 3
-
-nvidia-smi
-sleep 3
-
-export WANDB_MODE=offline
-
-python run_smoke_tests_v3.py --arms linear_ablation --stop-at-step 6500
+#2 +a -f-/opt/dlami/nvme/smoke_test_outputs_v3/linear_ablation.log
+#pull-linear-ablation-log
