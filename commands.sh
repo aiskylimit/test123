2c77f6ba29f09ee3d4cf59085899fac3f44b275c
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-07-11 09:10:28 +0000

    update

--- commands.sh
+++ commands.sh
@@ -1,2 +1,11 @@
-#2 +a
-#setup-env
+#1 +60+a
+#train-baseline
+eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
+sleep 3
+conda activate embeddings_hub
+sleep 3
+
+nvidia-smi
+sleep 3
+
+bash scripts/train_qwen3_0.6b_baseline.sh
